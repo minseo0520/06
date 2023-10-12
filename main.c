00c6b497047b9bc8e1c82a73ac949e7aa1d5e109
minseo0520 <141472390+minseo0520@users.noreply.github.com>
2023-10-12 17:05:23 +0900

    실습5

--- main.c
+++ main.c
@@ -3,15 +3,45 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int square(int a)
+int combination(int n, int r)
 {
-	return(a*a);
+	int div1, div2;
+	
+	div1 = factorial(n); //n!
+	div2 = factorial(n-r)*factorial(r); //(n-r)!n!
+	
+	//분모, 분자 나누기 연산
+	return (div1/div2); 
 }
 
-int main()
+int factorial(int n)
 {
-	int a=2;
-	a=square(a);
-	printf("a=%i\n", a);
+	int res = 1;
+	int i;
+	for(i=1; i<=n; i++)
+	 res=res*i;
+	return res;
 }
+
+int main(void)
+{
+	//1. 입력값 받음
+	int n, r;
+	int div1, div2; //div1-분자, div2-분모 
+	int result;
+	
+	printf("input n:");
+	scanf("%d", &n);
+	printf("input r:");
+	scanf("%d", &r); 
+	
+	//2. 분모/분자 계산
+	result = combination(n, r);
+	
+	//3. 최종값 출력 
+	printf("result : %i\n", result);
+}
+
+
+
 
